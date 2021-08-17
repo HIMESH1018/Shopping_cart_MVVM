a53ee9b665802451ba8a9f71bcb79b13e22b66a3
HIMESH1018 <pererahimesh39@gmail.com>
2021-08-17 23:16:23 +0530

    Last Commit 2 - Added animation with cart icon number

--- app/src/main/java/com/example/shoppingcart_test/MainActivity.java
+++ app/src/main/java/com/example/shoppingcart_test/MainActivity.java
@@ -4,25 +4,35 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.example.shoppingcart_test.Fragments.CartFragment;
 import com.example.shoppingcart_test.Fragments.HomeFragment;
+import com.example.shoppingcart_test.Models.CartItem;
+import com.example.shoppingcart_test.ViewModel.CartViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private FrameLayout fragmentContainer;
     private BottomNavigationView bottomMainNAV;
-//    private CartViewModel cartViewModel;
     public Fragment selectedfragment = null;
     FragmentManager fragmentManager;
+    private CartViewModel cartViewModel;
     // FloatingActionButton cart_img_icon;
     TextView cart_img_icon;
+    private Animation top;
     String TAG_HOME = "HOME";
     String TAG_CART = "CART";
 
@@ -34,7 +44,9 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         setupUI();
         setupListners();
 
+
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new HomeFragment()).commit();
+        cartnumber();
     }
 
     private void setupUI(){
@@ -78,5 +90,34 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         }
         return false;
+
+    }
+
+    private void cartnumber(){
+        top = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        cartViewModel = new ViewModelProvider(this).get(CartViewModel.class);
+        cartViewModel.getCart().observe(this, new Observer<List<CartItem>>() {
+            @Override
+            public void onChanged(List<CartItem> cartItems) {
+                if (cartItems.size() == 0) {
+
+                    cart_img_icon.setVisibility(View.GONE);
+
+                } else {
+                    int count = cartItems.size();
+                    if (count > 9) {
+
+                        cart_img_icon.setVisibility(View.VISIBLE);
+                        cart_img_icon.setText("9+");
+                        cart_img_icon.setAnimation(top);
+
+                    } else {
+                        cart_img_icon.setVisibility(View.VISIBLE);
+                        cart_img_icon.setText("" + count);
+                        cart_img_icon.setAnimation(top);
+                    }
+                }
+            }
+        });
     }
 }
